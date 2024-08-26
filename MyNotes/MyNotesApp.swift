1a446fab10732ef07fc9ae999a901f122f3c5e5f
Sravya <sravyachandrapati16@gmail.com>
2024-08-26 15:48:23 +1200

    Initial Commit

---
+++ MyNotes/MyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesApp.swift
+//  MyNotes
+//
+//  Created by Sudheer Vasepalli on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
